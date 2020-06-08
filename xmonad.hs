838e864ad5991f09c2cb11235cea2dd41355882a
tuatara <shiransv@hotmail.com>
2020-06-08 05:56:28 +0300

    xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -21,6 +21,11 @@ main = do
         } `additionalKeys`
         [ ((mod4Mask, xK_l), spawn "slock")
         , ((mod4Mask, xK_f), spawn "firefox")
+        , ((mod4Mask, xK_r), spawn "remmina")
+        , ((mod4Mask, xK_n), spawn "st -e nnn")
+        , ((mod4Mask, xK_n), spawn "st -e nnn")
+        , ((mod4Mask, xK_Prior), spawn "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo +5%")
+        , ((mod4Mask, xK_Next), spawn "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo -5%")
         , ((mod4Mask .|. shiftMask, xK_p), spawn "sudo poweroff")
         , ((mod4Mask .|. shiftMask, xK_r), spawn "sudo reboot")
         , ((0, xK_Print), spawn "flameshot gui")
